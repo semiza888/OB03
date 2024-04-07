08a5118d0eb065671bd21cf9f8996873aaa081f3
semiza888 <164627267+semiza888@users.noreply.github.com>
2024-04-07 17:51:16 +0300

    add subattributes to code

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # 1. Создайте базовый класс `Animal`, который будет содержать общие атрибуты
 # (например, `name`, `age`) и методы (`make_sound()`, `eat()`) для всех животных.
+# 2. Реализуйте наследование, создав подклассы `Bird`, `Mammal`, и `Reptile`,
+# которые наследуют от класса `Animal`.
+# Добавьте специфические атрибуты и переопределите методы, если требуется
+# (например, различный звук для `make_sound()`).
 
 # Базовый класс Animal
 class Animal:
@@ -13,6 +17,39 @@ class Animal:
 
     def eat(self):
         return f"{self.name} is eating"
+
+# Подкласс животных базового класса Animal
+
+class Bird(Animal):
+    def __init__(self, name, age, can_fly):
+        super().__init__(name, age)
+        self.can_fly = can_fly  # Может летать
+    def make_sound(self):
+        return f"{self.name} говорит 'Чирик-чирик!'"
+    def eat(self):
+        return f"{self.name} клюет зерна"
+
+class Mammal(Animal):
+    def __init__(self, name, age, has_fur):
+        super().__init__(name, age)
+        self.has_fur = has_fur    # Имеет шерсть
+    def make_sound(self):
+        pass
+
+class Cat(Animal):
+    def make_sound(self):
+        return f"{self.name} говорит 'Мяу-мяу!'"
+    def eat(self):
+        return f"{self.name} ест мясо"
+
+class Reptile(Animal):
+    def __init__(self, name, age, has_scales):
+        super().__init__(name, age)
+        self.has_scales = has_scales # имеют чешую на теле
+    def make_sound(self):
+        return f"{self.name} говорит 'Шшшшшш!'"
+    def eat(self):
+        return f"{self.name} ест мышей"
 
 
 
