6c6986cf9d658b555022a9ac680790620a0126d0
semiza888 <164627267+semiza888@users.noreply.github.com>
2024-04-07 18:29:04 +0300

    add polymorphism func

--- main.py
+++ main.py
@@ -4,11 +4,12 @@
 # которые наследуют от класса `Animal`.
 # Добавьте специфические атрибуты и переопределите методы, если требуется
 # (например, различный звук для `make_sound()`).
+# 3. Продемонстрируйте полиморфизм: создайте функцию `animal_sound(animals)`,
+# которая принимает список животных и вызывает метод `make_sound()` для каждого животного.
 
 # Базовый класс Animal
 class Animal:
     def __init__(self, name, age):
-
         self.name = name  # Имя животного
         self.age = age  # Возраст животного
 
@@ -19,37 +20,55 @@ class Animal:
         return f"{self.name} is eating"
 
 # Подкласс животных базового класса Animal
-
 class Bird(Animal):
     def __init__(self, name, age, can_fly):
         super().__init__(name, age)
         self.can_fly = can_fly  # Может летать
+
     def make_sound(self):
         return f"{self.name} говорит 'Чирик-чирик!'"
+
     def eat(self):
         return f"{self.name} клюет зерна"
 
 class Mammal(Animal):
     def __init__(self, name, age, has_fur):
         super().__init__(name, age)
-        self.has_fur = has_fur    # Имеет шерсть
-    def make_sound(self):
-        pass
+        self.has_fur = has_fur  # Имеет шерсть
 
-class Cat(Animal):
+    def make_sound(self):
+        return super().make_sound()
+
+class Cat(Mammal):
+    def __init__(self, name, age, has_fur=True):  # Все кошки имеющие шерсть по умолчанию
+        super().__init__(name, age, has_fur)
+
     def make_sound(self):
         return f"{self.name} говорит 'Мяу-мяу!'"
+
     def eat(self):
         return f"{self.name} ест мясо"
 
 class Reptile(Animal):
     def __init__(self, name, age, has_scales):
         super().__init__(name, age)
-        self.has_scales = has_scales # имеют чешую на теле
+        self.has_scales = has_scales  # Имеют чешую на теле
+
     def make_sound(self):
         return f"{self.name} говорит 'Шшшшшш!'"
+
     def eat(self):
         return f"{self.name} ест мышей"
 
+def animal_sound(animals):  # Демонстрация полиморфизма
+    for animal in animals:
+        print(animal.make_sound())
 
+animals = [  # Список животных разных видов
+    Bird("Чижик", 1, True),
+    Cat("Васька", 5, True),
+    Reptile("Гена", 4, True)
+]
 
+# Демонстрация работы функции
+animal_sound(animals)
